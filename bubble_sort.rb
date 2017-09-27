4c5fb457b4e1edafde7ae3a0390a14bb3d173efc
Jacob C Moore <jmooree30@gmail.com>
2017-09-26 20:00:45 -0400

    Update bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,69 +1,40 @@
 bubbles =  [9,8,7,6,5,4,3,2,1,0]
 
   def bubble_sort(array)  
-  
-  n = array.length
-
-  loop do 
-
-  sorted = false
-
-  (n-1).times do |i|
-
-    if array[i] > array[i+1]
-  
-      array[i], array[i+1] = array[i+1], array[i]
-        
-       sorted = true
-
-    end 
-   end
-
-   break if not sorted
-
+    n = array.length
+    loop do 
+      sorted = false
+      (n-1).times do |i|
+        if array[i] > array[i+1] 
+        array[i], array[i+1] = array[i+1], array[i]
+        sorted = true
+        end 
+      end
+      break if not sorted
+    end
+    return array 
   end
-
-   return array 
-
-end
 
 bubble_sort(bubbles)
 
-
-
-##############################################################################
-
-
  def bubble_sortby(array)  
-  
-  n = array.length
-
-  loop do 
-
-  sorted = false
-
-  (n-1).times do |i|
-
-    if array[i] > array[i+1]
-  
-      array[i], array[i+1] = array[i+1], array[i]
-        
-       sorted = true
-
-    end 
+   n = array.length
+   loop do 
+     sorted = false
+     (n-1).times do |i|
+       if array[i] > array[i+1]
+         array[i], array[i+1] = array[i+1], array[i]  
+         sorted = true
+       end 
+     end
+     break if not sorted
    end
-
-   break if not sorted
-
-  end
-
    return array 
-
-end
+ end
 
 bubble_sortby(["hi","hello","hey"]) do |left,right|
   left.length - right.length
-    end
+end
     
   
     
